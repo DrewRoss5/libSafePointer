561d5a2b3eb7bf7579754f007e5c0b658709e2b1
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-09-01 18:37:28 -0600

    Add testing for the SafeArr class

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -52,7 +52,69 @@ TEST(SafePtrTests, ExceptionTests){
 /***************************  
  TESTS FOR THE SafeArr CLASS
 *****************************/
-// TODO: Create tests for the SafeArr class
+
+// ensure basic functions work
+TEST(SafeArrTests, BasicTest){
+    SafeArr<int> arr(3);
+    arr.set(0, 1);
+    arr.set(1, 2);
+    arr.set(2, 3);
+    // validate that all data was set correctly
+    int len = arr.length();
+    for (int i = 0; i < len; i++)
+        EXPECT_EQ(arr[i], i + 1);
+}
+
+// ensure copying works
+TEST(SafeArrTests, CopyTests){
+    SafeArr<int> arr(2);
+    SafeArr<int> copy = arr.copy();
+    arr.set(0, 42);
+    copy.set(1, 24);
+    // validate that the data was succesfully updated by both
+    EXPECT_EQ(copy[0], 42);
+    EXPECT_EQ(arr[1], 24);
+}
+
+// ensure moving works
+TEST(SafeArrTests, MoveTests){
+    SafeArr<int> arr(5);
+    for (int i = 0; i < 5; i++)
+        arr.set(i, 5 - i);
+    SafeArr<int> moved = arr.move();
+    // ensure the data was transfered successfully
+    for (int i = 0; i < 5; i++)
+        EXPECT_EQ(moved[i], 5 - i);
+    // ensure the pointer was nulles successfully
+    bool is_nullptr = true;
+    if (arr.get_raw())
+        is_nullptr = false;
+    EXPECT_TRUE(is_nullptr);
+}
+
+// ensure exceptions are raised as appropriate
+TEST(SafeArrTests, ExceptionTests){
+    SafeArr<int> arr(2);
+    try{
+        int tmp = arr[0];
+    }
+    catch (std::runtime_error e){
+        EXPECT_FALSE(strcmp(e.what(), "Cannot retrieve value from an unitialized array"));
+    }
+    arr.set(0, 123);
+    try{
+        int tmp = arr[3];
+    }
+    catch (std::out_of_range e){
+        EXPECT_FALSE(strcmp(e.what(), "Invalid index"));
+    }
+    try{
+        int tmp = arr[-1];
+    }
+    catch (std::out_of_range e){
+        EXPECT_FALSE(strcmp(e.what(), "Invalid index"));
+    }
+}
 
 int main(int argc, char** argv){
     testing::InitGoogleTest(&argc, argv);
